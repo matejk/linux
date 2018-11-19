6c994d6b1939d88faf19f9aa6b83ab68bf3f6512
Alexandru Ardelean <alexandru.ardelean@analog.com>
2018-11-19 12:22:28 +0200

    drivers: jesd204: add initial skeleton subsystem
    This change adds the initial skeleton for the JESD204 subsystem. A lot of
    the code that will be placed in here will be coming from actual
    transceiver drivers, which implement quite the same init & operation things
    that should be put in a framework like this.
    
    Signed-off-by: Alexandru Ardelean <alexandru.ardelean@analog.com>

---
+++ drivers/jesd204/jesd204-priv.h
@@ -0,0 +1,18 @@
+/**
+ * The JESD204 framework
+ *
+ * Copyright (c) 2018 Analog Devices Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ */
+
+#ifndef _JESD204_PRIV_H_
+#define _JESD204_PRIV_H_
+
+#include <linux/jesd204/jesd204.h>
+
+extern struct bus_type jesd204_bus_type;
+
+#endif /* _JESD204_PRIV_H_ */
